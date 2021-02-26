4f07f1d610ceae6361b9cfbd22f251cb275bacb8
Yefer Martinez <131elyefer@gmail.com>
2021-02-26 20:05:15 +0100

    primer commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from routes import *
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+app.add_url_rule(user["registro_user"], view_func=user["registro_user_controllers"])
+app.add_url_rule(user["login_user"], view_func=user["login_user_controllers"])
+app.add_url_rule(user["adopcion_user"], view_func=user["adopcion_user_controllers"])
+app.add_url_rule(user["mascota_user"], view_func=user["mascota_user_controllers"])
